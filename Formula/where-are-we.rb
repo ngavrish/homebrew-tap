6684e96dddac57bad62fccaa0bbe5538b8d85acf
where-are-we release <apt@where-are-we.dev>
2026-09-03 21:55:16 +0000

    where-are-we 0.12.2

--- Formula/where-are-we.rb
+++ Formula/where-are-we.rb
@@ -3,8 +3,8 @@ class WhereAreWe < Formula
 
   desc "Index a codebase into a map an agent can read instead of grepping"
   homepage "https://github.com/ngavrish/where-are-we"
-  url "https://files.pythonhosted.org/packages/c5/cf/0210f794b18dc20b5bad557c84f2ee830dc376fc89e0ba7b06ab9e981c09/where_are_we-0.12.1.tar.gz"
-  sha256 "d03e4955915efa80646c79dde0ec8cbbb6500d366158de7b35f03f49b28d4a11"
+  url "https://files.pythonhosted.org/packages/00/de/99786d07462d7ae7bb80acbf7e24f57f295e9467b8cb1e54bf3fa0422fc4/where_are_we-0.12.2.tar.gz"
+  sha256 "0ffc0d65d9df8c1bcb8cd914c9d13ce9d0f2df0269194370e020c75510f0b521"
   license "MIT"
 
   depends_on "python@3.12"
